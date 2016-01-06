97e319a3b644181c3bad2169982800ce88045366
Andrew Brink <abrink27@gmail.com>
2016-01-06 07:40:29 +0000

    factory girl added

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
   test "quote show page" do
-  	quote = Quote.create(:author => "Andrew Brink", :saying => "Chill bro")
+  	quote = FactoryGirl.create(:quote)
   	get :show, :id => quote.id
   	assert_response :success
   end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
    test "unique tag" do
-   	quote = Quote.create(:author => 'Andrew Brink', :saying => "chill bro")
+   	quote = FactoryGirl.create(:quote, :author => 'Andrew Brink')
    	expected = 'AB#' + quote.id.to_s
     actual = quote.unique_tag
     assert_equal expected, actual
