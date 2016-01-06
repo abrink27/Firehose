a7486fda15c5439e96fa2c0c25c387de755c5998
Andrew Brink <abrink27@gmail.com>
2016-01-06 06:29:43 +0000

    test added

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "unique tag" do
+   	quote = Quote.create(:author => 'Andrew Brink', :saying => "chill bro")
+   	expected = 'AB#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
+
+   end
 end
